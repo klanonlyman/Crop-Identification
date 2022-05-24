24a0a166d5ff451dc2ac59889e76ec5b3e77ae66
TOM_NCKU <103729404+klanonlyman@users.noreply.github.com>
2022-05-24 12:19:52 +0800

    Update train.py

--- train.py
+++ train.py
@@ -14,7 +14,7 @@ from torchvision import transforms
 
 from my_dataset import MyDataSet
 from model import swin_large_patch4_window12_384_in22k as create_model
-from utils import read_split_data,train_one_epoch, evaluate
+from utils import read_split_data,one_epoch, evaluate
 
 if __name__ == '__main__':
   data_path="train"
@@ -31,7 +31,7 @@ if __name__ == '__main__':
       os.makedirs("./weights")
       
   tb_writer = SummaryWriter()
-  train_images_path, train_images_label, val_images_path, val_images_label = read_split_data(data_path)
+  train_images_path, train_images_label = read_split_data(data_path)
   img_size = 384
   data_transform = {
       "train": transforms.Compose([transforms.RandomResizedCrop(img_size),
@@ -56,7 +56,7 @@ if __name__ == '__main__':
   if weight_path != "":
       assert os.path.exists(weight_path), "weights file: '{}' not exist.".format(args.weights)
       weights_dict = torch.load(weight_path, map_location=device)["model"]
-      # 删除有关分类类别的权重
+
       for k in list(weights_dict.keys()):
           if "head" in k:
               del weights_dict[k]
@@ -65,8 +65,7 @@ if __name__ == '__main__':
   pg = [p for p in model.parameters() if p.requires_grad]
   optimizer = optim.AdamW(pg, lr=lr, weight_decay=1e-7)
   for epoch in range(epochs):
-      # train
-      train_loss, train_acc = train_one_epoch(model=model,
+      train_loss, train_acc = one_epoch(model=model,
                                               optimizer=optimizer,
                                               data_loader=train_loader,
                                               device=device,
